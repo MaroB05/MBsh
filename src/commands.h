49e1bc394e63903bf4c912157158f36467db26c0
MaroB05 <s-marwan.hussien@zewailcity.edu.eg>
2025-03-20 05:44:37 +0200

    error handling added in command cd

--- src/commands.h
+++ src/commands.h
@@ -12,7 +12,11 @@ int exit_(char* args[]);
 
 
 int cd_(char* args[]){
-  return chdir(args[1]);
+  if (chdir(args[1]) == -1){
+    perror("cd");
+    return -1;
+  }
+  return 0;
 }
 
 int exit_(char* args[]){
